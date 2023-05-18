6ec5f70b210ae39547f95279ec9213998b690f1a
Max Kaido <maximkaido@gmail.com>
2023-05-18 14:43:47 +0200

    wip

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,25 +1,32 @@
 import { ethers } from "hardhat";
+const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // We get the contract to deploy
+  const ElectionBet = await hre.ethers.getContractFactory("ElectionBet");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const [deployer] = await hre.ethers.getSigners();
+  const deployerAddress = await deployer.getAddress();
+  const CHAINLINK_ORACLE_ADDRESS = deployerAddress;
+  const JOB_ID = "my-job-id";
+  const FEE = hre.ethers.utils.parseEther("0.001");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const electionBet = await ElectionBet.deploy(
+    CHAINLINK_ORACLE_ADDRESS,
+    JOB_ID,
+    FEE
   );
+
+  await electionBet.deployed();
+
+  console.log("ElectionBet deployed to:", electionBet.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
