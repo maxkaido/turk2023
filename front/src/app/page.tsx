cd98f66a3df568c8d219ca8101344f0115cf4ff1
Max Kaido <maximkaido@gmail.com>
2023-05-19 20:10:25 +0200

    wip

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,6 +4,7 @@ import CountdownTimer from "./CountdownTimer";
 import { useEffect, useState } from "react";
 import ElectionBetArtifact from "../../artifacts/ElectionBet.json";
 import { ethers } from "ethers";
+import About from "./About";
 
 export default function Home() {
   // load contract from state
@@ -159,39 +160,7 @@ export default function Home() {
   const targetDate = new Date("2023-05-28T00:00:00");
   return (
     <main className="bg-gray-800 text-white min-h-screen text-white">
-      <div>
-        {account ? (
-          <div>
-            <h2>Account: {account}</h2>
-            <div>
-              <h3>Erdogan</h3>
-              <button onClick={getUserBetErdogan}>Get User Bet</button>
-              <button onClick={calculatePossibleWinErdogan}>
-                Calculate Possible Win
-              </button>
-              <button onClick={getTotalBetErdogan}>Get Total Bet</button>
-              <br />
-              <p>User Total Bet: {userTotalBetErdogan} ETH</p>
-              <p>Possible Win: {possibleWinErdogan} ETH</p>
-              <p>Total Bet on Erdogan: {totalBetErdogan} ETH</p>
-            </div>
-            <div>
-              <h3>Kemal</h3>
-              <button onClick={getUserBetKemal}>Get User Bet</button>
-              <button onClick={calculatePossibleWinKemal}>
-                Calculate Possible Win
-              </button>
-              <button onClick={getTotalBetKemal}>Get Total Bet</button>
-              <br />
-              <p>User Total Bet: {userTotalBetKemal} ETH</p>
-              <p>Possible Win: {possibleWinKemal} ETH</p>
-              <p>Total Bet on Kemal: {totalBetKemal} ETH</p>
-            </div>
-          </div>
-        ) : (
-          <button onClick={requestAccount}>Connect Wallet</button>
-        )}
-      </div>
+      <div></div>
       <nav className="bg-gray-900">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
@@ -299,58 +268,7 @@ export default function Home() {
           </div>
         ))}
       </div>
-      <div className="bg-gray-300 py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
-        <div className="relative max-w-xl mx-auto">
-          <div className="text-center">
-            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              About ElectionBetting
-            </h2>
-          </div>
-          <div className="mt-12">
-            <div className="text-center">
-              <p className="text-xl text-gray-500">
-                Welcome to the ElectionBetting Platform, where political
-                enthusiasts meet cutting-edge technology!
-              </p>
-              <p className="mt-4 text-xl text-gray-500">
-                Our platform leverages the power of blockchain and artificial
-                intelligence to provide a unique and thrilling experience.
-                Developed on the Ethereum network, ElectionBetting allows users
-                to place bets on the outcomes of political events - starting
-                with the 2023 Turkish General Elections.
-              </p>
-              <p className="mt-4 text-xl text-gray-500">
-                Our transparent and secure smart contracts are the backbone of
-                our platform. These contracts not only enable the betting
-                functionality but also manage payouts, all in a fully automated,
-                decentralized manner.
-              </p>
-              <p className="mt-4 text-xl text-gray-500">
-                To provide real-time and reliable data, we integrate with
-                Chainlink, a leading decentralized oracle network. Chainlink
-                nodes fetch election results from authoritative sources like
-                Wikipedia. We then utilize OpenAI`s language processing model,
-                GPT-4, to interpret the data and determine the winner.
-              </p>
-              <p className="mt-4 text-xl text-gray-500">
-                At ElectionBetting, we are passionate about blockchain
-                technology, fair elections, and innovative applications of AI.
-                We`re thrilled to offer a platform that marries these interests,
-                contributing to a transparent, decentralized, and exciting
-                future.
-              </p>
-              <p className="mt-4 text-xl text-gray-500">
-                Remember: it`s not just about guessing who will win, it`s about
-                participating in the process and the thrill of following these
-                significant events.
-              </p>
-              <p className="mt-4 text-xl text-gray-500 font-bold">
-                Join us in this new era of political engagement!
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <About />
     </main>
   );
 }
