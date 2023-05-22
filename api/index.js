0cb7035fb840910f64bb71ddab7952f4a9c01200
Max Kaido <maximkaido@gmail.com>
2023-05-22 23:10:02 +0200

    wip

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
+const express = require("express");
 const axios = require("axios");
 const { OpenAIApi } = require("openai");
+const expressRedisCache = require("express-redis-cache");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const cache = expressRedisCache({
+  expire: 24 * 60 * 60,
+  client: require("redis").createClient(
+    process.env.REDIS_URL || "redis://localhost:6379"
+  ),
+}); // Cache for 24 hours
 
 async function wikiContent(url) {
   try {
@@ -37,7 +48,7 @@ async function fetchWikiAndAskQuestion(url, question) {
     // Fetch the Wikipedia page
     let content = await wikiContent(url);
 
-    content = `I have read the following document: ${content}\n\n${question}}`;
+    content = `I have read the following document: ${content}\n\n${question}`;
     try {
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
@@ -52,7 +63,6 @@ async function fetchWikiAndAskQuestion(url, question) {
           },
         }
       );
-      console.log(response.data);
 
       return response.data.choices[0].message.content;
     } catch (error) {
@@ -63,10 +73,19 @@ async function fetchWikiAndAskQuestion(url, question) {
   }
 }
 
-// Usage
-fetchWikiAndAskQuestion(
-  "https://en.m.wikipedia.org/wiki/2023_Turkish_presidential_election",
-  "What are results of the election? return 0 if erdogan won, return 1 if kemal won, return 2 if not sure/unknown"
-)
-  .then((answer) => console.log(answer))
-  .catch((error) => console.error(error));
+app.get("/results", cache.route(), async (req, res) => {
+  try {
+    const answer = await fetchWikiAndAskQuestion(
+      "https://en.m.wikipedia.org/wiki/2023_Turkish_presidential_election",
+      "What is the outcome of the 2023 Turkish presidential election? Please respond with 'Erdogan' if Erdogan won, 'Kemal' if Kemal won, or 'n/a' if the result is not known or uncertain."
+    );
+    res.json({ answer });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
