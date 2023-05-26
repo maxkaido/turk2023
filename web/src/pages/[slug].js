661525de2b75affc029f95d0f31222a6eeb33b1d
Max Kaido <maximkaido@gmail.com>
2023-05-27 00:31:59 +0200

    wip

--- web/src/pages/[slug].js
+++ web/src/pages/[slug].js
@@ -9,10 +9,29 @@ export default function TemplatePage({ template }) {
   return (
     <>
       <Head>
-        <title>{template.title} - Jobot</title>
-        <meta name="description" content={template.description} />
-        <link rel="icon" href="/jobot_icon.png" type="image/png" />
-        <meta property="og:image" content="/jobot_meta.png" />
+        <title>Bet on the next Turkish president</title>
+        <meta name="description" content="Election Betting" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <div className="flex flex-col h-screen">
         <Navbar />
