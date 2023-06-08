18ed188bb266a9123abf365a8bf5ec4d169d0893
Max Kaido <maximkaido@gmail.com>
2023-06-08 13:59:05 +0200

    wip

--- web/src/pages/about.jsx
+++ web/src/pages/about.jsx
@@ -1,53 +1,54 @@
 import React from "react";
 
-const AboutElectionBetting = () => {
+const AboutWikiWager = () => {
   return (
     <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
       <div className="relative max-w-xl mx-auto">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            About ElectionBetting
+            About Wiki Wager
           </h2>
         </div>
         <div className="mt-12">
           <div className="text-center">
             <p className="text-lg text-gray-700">
-              Welcome to the ElectionBetting Platform, where political
-              enthusiasts meet cutting-edge technology!
+              Welcome to Wiki Wager, where betting meets the power of
+              technology!
             </p>
             <p className="mt-4 text-lg text-gray-700">
-              Our platform leverages the power of blockchain and artificial
-              intelligence to provide a unique and thrilling experience.
-              Developed on the Ethereum network, ElectionBetting allows users to
-              place bets on the outcomes of political events - starting with the
+              Our platform is built on the Ethereum network and harnesses the
+              potential of blockchain and artificial intelligence to provide an
+              unparalleled betting experience. At Wiki Wager, you can place bets
+              on the outcomes of various real-world events, starting with the
               2023 Turkish General Elections.
             </p>
             <p className="mt-4 text-lg text-gray-700">
-              Our transparent and secure smart contracts are the backbone of our
-              platform. These contracts not only enable the betting
-              functionality but also manage payouts, all in a fully automated,
-              decentralized manner.
+              Our platform is backed by transparent and secure smart contracts
+              that not only enable betting functionality but also handle payouts
+              in a fully automated and decentralized manner.
             </p>
             <p className="mt-4 text-lg text-gray-700">
-              To provide real-time and reliable data, we integrate with
+              To ensure real-time and reliable data, we have integrated with
               Chainlink, a leading decentralized oracle network. Chainlink nodes
               fetch election results from authoritative sources like Wikipedia.
-              We then utilize OpenAI`s language processing model, GPT-4, to
-              interpret the data and determine the winner.
+              We then utilize the power of OpenAI`s language processing model,
+              GPT-3.5, to interpret the data and determine the winners
+              accurately.
             </p>
             <p className="mt-4 text-lg text-gray-700">
-              At ElectionBetting, we are passionate about blockchain technology,
-              fair elections, and innovative applications of AI. We`re thrilled
-              to offer a platform that marries these interests, contributing to
-              a transparent, decentralized, and exciting future.
+              At Wiki Wager, we are driven by our passion for blockchain
+              technology, fair elections, and the innovative applications of AI.
+              We are excited to offer a platform that combines these interests,
+              contributing to a transparent, decentralized, and thrilling
+              future.
             </p>
             <p className="mt-4 text-lg text-gray-700">
-              Remember: it`s not just about guessing who will win, it`s about
-              participating in the process and the thrill of following these
-              significant events.
+              Remember, Wiki Wager is not just about predicting winners; it`s
+              about actively participating in the process and experiencing the
+              excitement of these significant events firsthand.
             </p>
             <p className="mt-4 text-lg text-gray-700 font-bold">
-              Join us in this new era of political engagement!
+              Join us in this new era of engaging and transparent betting!
             </p>
           </div>
         </div>
@@ -56,4 +57,4 @@ const AboutElectionBetting = () => {
   );
 };
 
-export default AboutElectionBetting;
+export default AboutWikiWager;
